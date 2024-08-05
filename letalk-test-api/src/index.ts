3b28dbf730e193170ec19655d6e0f77a22e3172d
eduardoamaral-dev <eduardo.lopes.amaral@outlook.com>
2024-08-05 11:54:23 -0300

    🩹 using cors first

--- letalk-test-api/src/index.ts
+++ letalk-test-api/src/index.ts
@@ -4,12 +4,13 @@ import { router } from './router';
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use('/', router);
-app.use(cors());
+
 
 
 app.listen(PORT, () => {
